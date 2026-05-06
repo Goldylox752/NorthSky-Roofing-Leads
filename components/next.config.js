8f96ddd9ef994f3f8ff02854ffbca83d2cf077d9
Byron Sanche <261788158+Goldylox752@users.noreply.github.com>
2026-05-06 11:31:57 -0600

    Create next.config.js

---
+++ components/next.config.js
@@ -0,0 +1,14 @@
+{
+  "compilerOptions": {
+    "baseUrl": ".",
+    "paths": {
+      "@/*": ["./*"]
+    },
+    "jsx": "preserve",
+    "moduleResolution": "bundler",
+    "resolveJsonModule": true,
+    "esModuleInterop": true,
+    "strict": false,
+    "skipLibCheck": true
+  }
+}
